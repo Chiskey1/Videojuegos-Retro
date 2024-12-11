e8033de91d47856cd37f8daacae941d2590a3b86
Chiskey <chiskeyyy2565@gmail.com>
2024-12-10 22:33:49 -0600

    Se agrega el Proyecto

---
+++ inicio2.js
@@ -0,0 +1,76 @@
+//----------------------------
+// Selección de Producto
+//----------------------------
+
+let mostrador = document.getElementById("mostrador");
+let seleccion = document.getElementById("seleccion");
+let imgSeleccionada = document.getElementById("img");
+let modeloSeleccionado = document.getElementById("modelo");
+let descripSeleccionada = document.getElementById("descripcion");
+let precioSeleccionado = document.getElementById("precio");
+
+function cargar(item) {
+    quitarBordes();
+    mostrador.style.width = "60%";
+    seleccion.style.width = "40%";
+    seleccion.style.opacity = "1";
+    item.style.border = "2px solid red";
+
+    imgSeleccionada.src = item.getElementsByTagName("img")[0].src;
+    modeloSeleccionado.innerHTML = item.getElementsByTagName("p")[0].innerHTML;
+    descripSeleccionada.innerHTML = "Descripción del modelo ";
+    precioSeleccionado.innerHTML = item.getElementsByTagName("span")[0].innerHTML;
+}
+
+function cerrar() {
+    mostrador.style.width = "100%";
+    seleccion.style.width = "0%";
+    seleccion.style.opacity = "0";
+    quitarBordes();
+}
+
+function quitarBordes() {
+    var items = document.getElementsByClassName("item");
+    for (i = 0; i < items.length; i++) {
+        items[i].style.border = "none";
+    }
+}
+
+//----------------------------
+// Inicialización de Swiper (Carrusel)
+//----------------------------
+
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+});
+
+var swiper2 = new Swiper(".mySwiper-2", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        520: {
+            slidesPerView: 2
+        },
+        950: {
+            slidesPerView: 3
+        },
+    }
+});
